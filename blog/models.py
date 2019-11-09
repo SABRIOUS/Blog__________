39faa9c3bdbc556f07efbd4b43c94262ca31d8b1
SABRIOUS <ahmedsabrious@gmail.com>
2019-11-10 00:04:35 +0200

    add List & Detail Views

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 # Create your models here.
 # instead of doing Post.objects.all or something else we can use now Post.published.filter(title = ....)
 
@@ -31,6 +33,13 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.publish.year,
+                                                self.publish.strftime('%m'),
+                                                self.publish.strftime('%d'),
+                                                self.slug])
+
 
     def __str__(self):
         return self.title
