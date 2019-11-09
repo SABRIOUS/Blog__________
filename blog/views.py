882d546335ca9c3b6419e2e7c67ec1dd6ad1d977
SABRIOUS <ahmedsabrious@gmail.com>
2019-11-10 00:57:45 +0200

    add templates

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,8 @@ def post_list(request):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
-                                    status='published',
-                                    published__year=year,
-                                    published__month=month,
-                                    published__day=day)
-    return render(request,'blog/post/detail.html',{'post':post})
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
